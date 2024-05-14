21774ebbc212df511bfe98fab811fa720718006f
Bui Cong Thuan <thuanbui2611@gmail.com>
2024-05-14 18:50:03 +0700

    set dbset

--- T-Shop.Infrastructure/Persistence/ApplicationContext.cs
+++ T-Shop.Infrastructure/Persistence/ApplicationContext.cs
@@ -13,7 +13,13 @@ namespace T_Shop.Infrastructure.Persistence
         }
 
         public DbSet<Product> Products { get; set; }
-        public DbSet<Category> Categories { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<Model> Models { get; set; }
+        public DbSet<TypeProduct> Types { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -35,7 +41,6 @@ namespace T_Shop.Infrastructure.Persistence
 
         private static void SeedDatabase(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Category>().HasData(SeedDatabaseConstant.Default_Category);
             modelBuilder.Entity<Product>().HasData(SeedDatabaseConstant.Default_Product);
         }
 
--- T-Shop.Infrastructure/Persistence/IApplicationContext.cs
+++ T-Shop.Infrastructure/Persistence/IApplicationContext.cs
@@ -6,6 +6,11 @@ namespace T_Shop.Infrastructure.Persistence
     public interface IApplicationContext
     {
         DbSet<Product> Products { get; set; }
-        DbSet<Category> Categories { get; set; }
+        DbSet<Brand> Brands { get; set; }
+        DbSet<Color> Colors { get; set; }
+        DbSet<Image> Images { get; set; }
+        DbSet<ProductImage> ProductImages { get; set; }
+        DbSet<Model> Models { get; set; }
+        DbSet<TypeProduct> Types { get; set; }
     }
 }
