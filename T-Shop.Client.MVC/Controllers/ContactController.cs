433649794d33fbcabd268fcbf5e359e24f10907e
Bui Cong Thuan <thuanbui2611@gmail.com>
2024-05-25 00:37:25 +0700

    basic html

---
+++ T-Shop.Client.MVC/Controllers/ContactController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace T_Shop.Client.MVC.Controllers;
+public class ContactController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
