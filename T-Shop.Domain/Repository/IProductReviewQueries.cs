0cc462adc0e3812fec322c841d74781244eb1968
Thuan <thuanbui2611@gmail.com>
2024-06-02 00:38:36 +0700

    add pagination for product reviews

--- T-Shop.Domain/Repository/IProductReviewQueries.cs
+++ T-Shop.Domain/Repository/IProductReviewQueries.cs
@@ -1,9 +1,10 @@
 ﻿using T_Shop.Domain.Entity;
+using T_Shop.Shared.DTOs.Pagination;
 
 namespace T_Shop.Domain.Repository;
 public interface IProductReviewQueries
 {
     Task<int> CountTotalReviewByProductId(Guid productID);
     Task<decimal> GetAverageRatingByProductId(Guid productID);
-    Task<List<ProductReview>> GetAllProductReviewByProductIdAsync(Guid productID);
+    Task<(List<ProductReview>, PaginationMetaData)> GetProductReviewsByProductIdAsync(Guid productID, PaginationRequestModel pagination);
 }
--- T-Shop.Infrastructure/Data/Queries/ProductReviewQueries.cs
+++ T-Shop.Infrastructure/Data/Queries/ProductReviewQueries.cs
@@ -2,6 +2,7 @@
 using T_Shop.Domain.Entity;
 using T_Shop.Domain.Repository;
 using T_Shop.Infrastructure.Persistence;
+using T_Shop.Shared.DTOs.Pagination;
 
 namespace T_Shop.Infrastructure.Data.Queries;
 public class ProductReviewQueries : BaseQuery<ProductReview>, IProductReviewQueries
@@ -16,13 +17,21 @@ public class ProductReviewQueries : BaseQuery<ProductReview>, IProductReviewQuer
         return await dbSet.CountAsync(x => x.ProductID.Equals(productID));
     }
 
-    public async Task<List<ProductReview>> GetAllProductReviewByProductIdAsync(Guid productID)
+    public async Task<(List<ProductReview>, PaginationMetaData)> GetProductReviewsByProductIdAsync(Guid productID, PaginationRequestModel pagination)
     {
-        return await dbSet
+        var totalItemCount = await dbSet
+            .Where(pr => pr.ProductID.Equals(productID))
+            .CountAsync();
+
+        var reviewsOfProduct = await dbSet
                 .Include(x => x.ProductReviewImages)
                 .Where(x => x.ProductID.Equals(productID))
-                .OrderBy(x => x.CreatedAt)
+                .OrderByDescending(x => x.CreatedAt)
+                .Skip(pagination.pageSize * (pagination.pageNumber - 1))
+                .Take(pagination.pageSize)
                 .ToListAsync();
+        var paginationMetaData = new PaginationMetaData(totalItemCount, pagination.pageSize, pagination.pageNumber);
+        return (reviewsOfProduct, paginationMetaData);
     }
 
     public async Task<decimal> GetAverageRatingByProductId(Guid productID)
