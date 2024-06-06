f78c791b8e763486b909c3b1bbcdd901bcc76c2a
Bui Cong Thuan <thuanbui2611@gmail.com>
2024-06-06 09:58:39 +0700

    update cache when lock/unlock product

--- T-Shop.Application/Features/Products/Commands/LockOrUnlockProduct/LockOrUnlockProductCommandHandler.cs
+++ T-Shop.Application/Features/Products/Commands/LockOrUnlockProduct/LockOrUnlockProductCommandHandler.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using LazyCache;
+using MediatR;
+using T_Shop.Application.Common.Constants;
 using T_Shop.Domain.Exceptions;
 using T_Shop.Domain.Repository;
 
@@ -8,10 +10,15 @@ public class LockOrUnlockProductCommandHandler : IRequestHandler<LockOrUnlockPro
     private readonly IGenericRepository<Domain.Entity.Product> _productRepository;
     private readonly IUnitOfWork _unitOfWork;
 
-    public LockOrUnlockProductCommandHandler(IUnitOfWork unitOfWork)
+    private readonly IAppCache _cache;
+    private CacheKeyConstants _cacheKeyConstants;
+
+    public LockOrUnlockProductCommandHandler(IUnitOfWork unitOfWork, IAppCache cache, CacheKeyConstants cacheKeyConstants)
     {
         _unitOfWork = unitOfWork;
         _productRepository = unitOfWork.GetBaseRepo<Domain.Entity.Product>();
+        _cache = cache;
+        _cacheKeyConstants = cacheKeyConstants;
     }
 
     public async Task<bool> Handle(LockOrUnlockProductCommand request, CancellationToken cancellationToken)
@@ -20,8 +27,29 @@ public class LockOrUnlockProductCommandHandler : IRequestHandler<LockOrUnlockPro
         if (product == null) throw new NotFoundException("Product not found!");
 
         product.IsOnStock = !product.IsOnStock;
+
         _productRepository.Update(product);
         await _unitOfWork.CompleteAsync();
+
+        UpdateExistedCache(product.Id);
+
         return true;
+    }
+
+    private async void UpdateExistedCache(Guid productId)
+    {
+        var key = _cacheKeyConstants.ProductCacheKey;
+        var cacheValues = await _cache.GetAsync<List<Domain.Entity.Product>>(key);
+        if (cacheValues != null)
+        {
+            var productInCache = cacheValues.Find(t => t.Id.Equals(productId));
+            if (productInCache == null) return;
+
+            cacheValues.RemoveAll(t => t.Id.Equals(productInCache?.Id));
+
+            productInCache.IsOnStock = !productInCache.IsOnStock;
+            cacheValues.Add(productInCache);
+            _cache.Add(key, cacheValues);
+        }
     }
 }
