7779d9f15a16732f7fef88c8ee1f24c3f387105e
Thuan <thuanbui2611@gmail.com>
2024-06-08 11:09:38 +0700

    Color: unit test for queries

---
+++ T-Shop.Application.XUnitTest/Handler/Color/GetColorById/GetColorByIdQueryHandlerTest.cs
@@ -0,0 +1,55 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using Moq;
+using T_Shop.Application.Features.Color.Queries.GetColorById;
+using T_Shop.Domain.Exceptions;
+using T_Shop.Domain.Repository;
+using T_Shop.Shared.DTOs.Color.ResponseModel;
+
+namespace T_Shop.Application.XUnitTest.Handler.Color.GetColorById;
+public class GetColorByIdQueryHandlerTest : TestSetup
+{
+    private readonly Mock<IColorQueries> _mockColorQueries;
+    private readonly GetColorByIdQueryHandler _handler;
+
+    public GetColorByIdQueryHandlerTest()
+    {
+        _mockColorQueries = new Mock<IColorQueries>();
+        _handler = new GetColorByIdQueryHandler(_mapperConfig, _mockColorQueries.Object);
+    }
+
+    [Fact]
+    public async Task Should_Return_ColorResponseModel_OnExistingColorId()
+    {
+        // Arrange
+        var expectedColor = _fixture.Create<Domain.Entity.Color>();
+        var expectedResult = _mapperConfig.Map<ColorResponseModel>(expectedColor);
+        _mockColorQueries.Setup(x => x.GetColorByIdAsync(expectedColor.Id)).ReturnsAsync(expectedColor);
+
+        var request = new GetColorByIdQuery { ID = expectedColor.Id };
+
+        // Act
+        var result = await _handler.Handle(request, CancellationToken.None);
+
+        // Assert
+        _mockColorQueries.Verify(x => x.GetColorByIdAsync(expectedColor.Id), Times.Once);
+        result.Should().BeEquivalentTo(expectedResult);
+    }
+
+    [Fact]
+    public async Task Should_Throw_BadRequestException_OnNonExistingColorId()
+    {
+        // Arrange
+        var nonExistingColorId = Guid.NewGuid();
+        _mockColorQueries.Setup(x => x.GetColorByIdAsync(nonExistingColorId)).ReturnsAsync((Domain.Entity.Color)null);
+
+        var request = new GetColorByIdQuery { ID = nonExistingColorId };
+
+        // Act & Assert
+        await FluentActions.Invoking(() => new GetColorByIdQueryHandler(_mapperConfig, _mockColorQueries.Object).Handle(request, CancellationToken.None))
+            .Should().ThrowAsync<NotFoundException>()
+            .WithMessage("Color can not found");
+
+        _mockColorQueries.Verify(x => x.GetColorByIdAsync(nonExistingColorId), Times.Once);
+    }
+}
---
+++ T-Shop.Application.XUnitTest/Handler/Color/GetColors/GetColorsQueryHandlerTest.cs
@@ -0,0 +1,79 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using Microsoft.Extensions.Caching.Memory;
+using Moq;
+using T_Shop.Application.Features.Color.Queries.GetColors;
+using T_Shop.Domain.Repository;
+using T_Shop.Shared.DTOs.Color.ResponseModel;
+
+namespace T_Shop.Application.XUnitTest.Handler.Color.GetColors;
+public class GetColorsQueryHandlerTest : TestSetup
+{
+    private readonly Mock<IColorQueries> _mockColorQueries;
+    private readonly GetColorsQueryHandler _handler;
+
+    public GetColorsQueryHandlerTest()
+    {
+        _mockColorQueries = new Mock<IColorQueries>();
+        _handler = new GetColorsQueryHandler(_mapperConfig, _mockColorQueries.Object, _cacheMock.Object, _cacheKeyConstants);
+    }
+
+    [Fact]
+    public async Task Should_Return_MappedColors_FromCache_WhenCacheExists()
+    {
+        // Arrange
+        var expectedColors = _fixture.CreateMany<Domain.Entity.Color>().ToList();
+        var expectedResult = _mapperConfig.Map<List<ColorResponseModel>>(expectedColors);
+        var cacheKey = _cacheKeyConstants.ColorCacheKey;
+        _cacheMock.Setup(c => c.GetOrAddAsync(
+           cacheKey,
+           It.IsAny<Func<ICacheEntry, Task<List<Domain.Entity.Color>>>>(),
+           It.IsAny<MemoryCacheEntryOptions>()))
+           .ReturnsAsync(expectedColors);
+
+        // Act
+        var result = await _handler.Handle(new GetColorsQuery(), CancellationToken.None);
+
+        // Assert
+        _cacheMock.Verify(c => c.GetOrAddAsync(
+            cacheKey,
+            It.IsAny<Func<ICacheEntry, Task<List<Domain.Entity.Color>>>>(),
+            It.IsAny<MemoryCacheEntryOptions>()));
+
+        _mockColorQueries.Verify(x => x.GetColorsAsync(), Times.Never); // Not called since cache is used
+        result.Should().BeEquivalentTo(expectedResult);
+        _cacheKeyConstants.CacheKeyList.Should().Contain(cacheKey);
+    }
+
+    [Fact]
+    public async Task Should_Return_MappedColors_FromColorQueries_AndCache_WhenCacheEmpty()
+    {
+        // Arrange
+        var expectedColors = _fixture.CreateMany<Domain.Entity.Color>().ToList();
+        var expectedResult = _mapperConfig.Map<List<ColorResponseModel>>(expectedColors);
+        var cacheKey = _cacheKeyConstants.ColorCacheKey;
+        _cacheMock.Setup(c => c.GetOrAddAsync(
+            It.IsAny<string>(),
+            It.IsAny<Func<ICacheEntry, Task<List<Domain.Entity.Color>>>>(),
+            It.IsAny<MemoryCacheEntryOptions>()))
+            .Returns(async (string key, Func<ICacheEntry, Task<List<Domain.Entity.Color>>> valueFactory, MemoryCacheEntryOptions options) =>
+            {
+                var colors = await valueFactory(new Mock<ICacheEntry>().Object);
+                return colors;
+            });
+
+        _mockColorQueries.Setup(x => x.GetColorsAsync()).ReturnsAsync(expectedColors);
+
+        // Act
+        var result = await _handler.Handle(new GetColorsQuery(), CancellationToken.None);
+
+        // Assert
+        _cacheMock.Verify(c => c.GetOrAddAsync(
+             It.IsAny<string>(),
+            It.IsAny<Func<ICacheEntry, Task<List<Domain.Entity.Color>>>>(),
+            It.IsAny<MemoryCacheEntryOptions>()), Times.Once);
+        _mockColorQueries.Verify(x => x.GetColorsAsync(), Times.Once);
+        result.Should().BeEquivalentTo(expectedResult);
+        _cacheKeyConstants.CacheKeyList.Should().Contain(cacheKey);
+    }
+}
