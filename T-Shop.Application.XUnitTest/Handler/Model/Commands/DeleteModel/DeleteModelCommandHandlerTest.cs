ef94b4a777a7c2cb683a94b3ec85301f3ef2bae9
Thuan <thuanbui2611@gmail.com>
2024-06-09 21:11:48 +0700

    Model: unit test for update/delete

---
+++ T-Shop.Application.XUnitTest/Handler/Model/Commands/DeleteModel/DeleteModelCommandHandlerTest.cs
@@ -0,0 +1,70 @@
+﻿using AutoFixture;
+using LazyCache;
+using Microsoft.Extensions.Caching.Memory;
+using Moq;
+using T_Shop.Application.Features.ModelProduct.Commands.DeleteModelProduct;
+using T_Shop.Domain.Exceptions;
+using T_Shop.Domain.Repository;
+
+namespace T_Shop.Application.XUnitTest.Handler.Model.Commands.DeleteModel;
+public class DeleteModelCommandHandlerTest : TestSetup
+{
+    private readonly Mock<IGenericRepository<Domain.Entity.Model>> _colorRepositoryMock;
+    private readonly DeleteModelProductCommandHandler _handler;
+
+    public DeleteModelCommandHandlerTest()
+    {
+        _colorRepositoryMock = new Mock<IGenericRepository<Domain.Entity.Model>>();
+        _mockUnitOfWork.Setup(uow => uow.GetBaseRepo<Domain.Entity.Model>()).Returns(_colorRepositoryMock.Object);
+
+        _handler = new DeleteModelProductCommandHandler(_mockUnitOfWork.Object, _cacheMock.Object, _cacheKeyConstants);
+        _mockUnitOfWork.Setup(uow => uow.GetBaseRepo<Domain.Entity.Model>()).Returns(_colorRepositoryMock.Object);
+    }
+    [Fact]
+    public async Task Should_DeleteModel_UpdateCache_AndReturnTrue_OnExistingModel()
+    {
+        // Arrange
+        var request = _fixture.Create<DeleteModelProductCommand>();
+        var existingModel = _fixture.Build<Domain.Entity.Model>()
+            .With(b => b.Id, request.ID)
+            .Create();
+        _colorRepositoryMock.Setup(x => x.GetById(request.ID)).ReturnsAsync(existingModel);
+        _mockUnitOfWork.Setup(x => x.CompleteAsync()).Returns(Task.CompletedTask);
+        var cacheValues = _fixture.CreateMany<Domain.Entity.Model>().ToList();
+        _cacheMock.Setup(c => c.GetAsync<List<Domain.Entity.Model>>(_cacheKeyConstants.ModelCacheKey)).ReturnsAsync(cacheValues);
+
+        // Mocking DefaultCachePolicy and BuildOptions
+        var cachePolicyMock = new Mock<CacheDefaults>();
+
+        _cacheMock.Setup(c => c.DefaultCachePolicy).Returns(cachePolicyMock.Object);
+
+        // Mock the Add method for IAppCache to avoid NullReferenceException
+        _cacheMock.Setup(c => c.Add(_cacheKeyConstants.ModelCacheKey, It.IsAny<List<Domain.Entity.Model>>(), It.IsAny<MemoryCacheEntryOptions>()))
+                  .Verifiable();
+        // Act
+        var result = await _handler.Handle(request, CancellationToken.None);
+
+        // Assert
+        _colorRepositoryMock.Verify(x => x.GetById(request.ID), Times.Once);
+        _colorRepositoryMock.Verify(x => x.Delete(request.ID), Times.Once);
+        _mockUnitOfWork.Verify(x => x.CompleteAsync(), Times.Once);
+        _cacheMock.Verify(x => x.GetAsync<List<Domain.Entity.Model>>(_cacheKeyConstants.ModelCacheKey), Times.Once);
+        _cacheMock.Verify(x => x.Add(_cacheKeyConstants.ModelCacheKey, It.IsAny<List<Domain.Entity.Model>>(), It.IsAny<MemoryCacheEntryOptions>()), Times.Once);
+        Assert.True(result);
+    }
+
+    [Fact]
+    public async Task Should_ThrowNotFoundException_OnNonexistentModel()
+    {
+        // Arrange
+        var request = _fixture.Create<DeleteModelProductCommand>();
+        _colorRepositoryMock.Setup(x => x.GetById(request.ID)).ReturnsAsync((Domain.Entity.Model)null);
+
+        // Act & Assert
+        await Assert.ThrowsAsync<NotFoundException>(async () => await _handler.Handle(request, CancellationToken.None));
+        _colorRepositoryMock.Verify(x => x.Delete(It.IsAny<Guid>()), Times.Never);
+        _mockUnitOfWork.Verify(x => x.CompleteAsync(), Times.Never);
+        _cacheMock.Verify(x => x.GetAsync<List<Domain.Entity.Model>>(_cacheKeyConstants.ModelCacheKey), Times.Never);
+        _cacheMock.Verify(x => x.Add(It.IsAny<string>(), It.IsAny<object>(), It.IsAny<MemoryCacheEntryOptions>()), Times.Never);
+    }
+}
---
+++ T-Shop.Application.XUnitTest/Handler/Model/Commands/UpdateModel/UpdateModelCommandHandlerTest.cs
@@ -0,0 +1,91 @@
+﻿using AutoFixture;
+using LazyCache;
+using Microsoft.Extensions.Caching.Memory;
+using Moq;
+using T_Shop.Application.Features.ModelProduct.Commands.UpdateModelProduct;
+using T_Shop.Domain.Exceptions;
+using T_Shop.Domain.Repository;
+
+namespace T_Shop.Application.XUnitTest.Handler.Model.Commands.UpdateModel;
+public class UpdateModelCommandHandlerTest : TestSetup
+{
+    private readonly Mock<IModelQueries> _mockModelQueries;
+    private readonly Mock<IBrandQueries> _mockBrandQueries;
+    private readonly Mock<IGenericRepository<Domain.Entity.Model>> _modelRepositoryMock;
+    private readonly UpdateModelProductCommandHandler _handler;
+
+    public UpdateModelCommandHandlerTest()
+    {
+        _modelRepositoryMock = new Mock<IGenericRepository<Domain.Entity.Model>>();
+        _mockUnitOfWork.Setup(uow => uow.GetBaseRepo<Domain.Entity.Model>()).Returns(_modelRepositoryMock.Object);
+        _mockModelQueries = new Mock<IModelQueries>();
+        _mockBrandQueries = new Mock<IBrandQueries>();
+        _handler = new UpdateModelProductCommandHandler(_mockModelQueries.Object, _mockUnitOfWork.Object, _mapperConfig, _mockBrandQueries.Object, _cacheMock.Object, _cacheKeyConstants);
+    }
+
+    [Fact]
+    public async Task Should_UpdateModel_UpdateCache_AndReturnResponse_OnValidData()
+    {
+        // Arrange
+        var request = _fixture.Create<UpdateModelProductCommand>();
+        var existingModel = _fixture.CreateMany<Domain.Entity.Model>().ToList(); // No existing model with same year
+
+        _mockModelQueries.Setup(x => x.GetModelsByNameAsync(request.Name)).ReturnsAsync(existingModel);
+        var brand = _fixture.Create<Domain.Entity.Brand>();
+        _mockBrandQueries.Setup(x => x.GetBrandByIdAsync(request.BrandID)).ReturnsAsync(brand);
+        _mockUnitOfWork.Setup(x => x.CompleteAsync()).Returns(Task.CompletedTask);
+
+        var mappedModel = _mapperConfig.Map<Domain.Entity.Model>(request); // Mapped model
+        var cacheValues = _fixture.CreateMany<Domain.Entity.Model>().ToList();
+        cacheValues.Add(mappedModel);
+        _cacheMock.Setup(c => c.GetAsync<List<Domain.Entity.Model>>(_cacheKeyConstants.ModelCacheKey)).ReturnsAsync(cacheValues);
+        // Mocking DefaultCachePolicy and BuildOptions
+        var cachePolicyMock = new Mock<CacheDefaults>();
+
+        _cacheMock.Setup(c => c.DefaultCachePolicy).Returns(cachePolicyMock.Object);
+        _cacheMock.Setup(c => c.Add(It.IsAny<string>(), It.IsAny<List<Domain.Entity.Model>>(), It.IsAny<MemoryCacheEntryOptions>()))
+                 .Verifiable();
+        // Act
+        var result = await _handler.Handle(request, CancellationToken.None);
+
+        // Assert
+        _mockModelQueries.Verify(x => x.GetModelsByNameAsync(request.Name), Times.Once);
+        _mockBrandQueries.Verify(x => x.GetBrandByIdAsync(request.BrandID), Times.Once);
+
+        _mockUnitOfWork.Verify(x => x.CompleteAsync(), Times.Once);
+        _cacheMock.Verify(x => x.GetAsync<List<Domain.Entity.Model>>(_cacheKeyConstants.ModelCacheKey), Times.Once);
+        _cacheMock.Verify(x => x.Add(_cacheKeyConstants.ModelCacheKey, It.IsAny<List<Domain.Entity.Model>>(), It.IsAny<MemoryCacheEntryOptions>()), Times.Once);
+        Assert.NotNull(result); // Assuming Domain.Entity.ModelProductResponseModel has properties mapped from Model
+    }
+
+    [Fact]
+    public async Task Should_ThrowConflictException_OnExistingModelWithSameYear()
+    {
+        // Arrange
+        var request = _fixture.Create<UpdateModelProductCommand>();
+        var existingModel = new List<Domain.Entity.Model>() { new Domain.Entity.Model { Name = request.Name, Year = request.Year } };
+        _mockModelQueries.Setup(x => x.GetModelsByNameAsync(request.Name)).ReturnsAsync(existingModel);
+
+        // Act & Assert
+        await Assert.ThrowsAsync<ConflictException>(async () => await _handler.Handle(request, CancellationToken.None));
+        _modelRepositoryMock.Verify(x => x.Update(It.IsAny<Domain.Entity.Model>()), Times.Never);
+        _mockUnitOfWork.Verify(x => x.CompleteAsync(), Times.Never);
+        _cacheMock.Verify(x => x.GetAsync<List<Domain.Entity.Model>>(_cacheKeyConstants.ModelCacheKey), Times.Never);
+        _cacheMock.Verify(x => x.Add(It.IsAny<string>(), It.IsAny<object>(), It.IsAny<MemoryCacheEntryOptions>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task Should_ThrowNotFoundException_OnNonexistentBrand()
+    {
+        // Arrange
+        var request = _fixture.Create<UpdateModelProductCommand>();
+        _mockModelQueries.Setup(x => x.GetModelsByNameAsync(request.Name)).ReturnsAsync(new List<Domain.Entity.Model>());
+        _mockBrandQueries.Setup(x => x.GetBrandByIdAsync(request.BrandID)).ReturnsAsync((Domain.Entity.Brand)null);
+        // Act & Assert
+        await Assert.ThrowsAsync<NotFoundException>(async () => await _handler.Handle(request, CancellationToken.None));
+        _modelRepositoryMock.Verify(x => x.Update(It.IsAny<Domain.Entity.Model>()), Times.Never);
+        _mockUnitOfWork.Verify(x => x.CompleteAsync(), Times.Never);
+        _cacheMock.Verify(x => x.GetAsync<List<Domain.Entity.Model>>(_cacheKeyConstants.ModelCacheKey), Times.Never);
+        _cacheMock.Verify(x => x.Add(It.IsAny<string>(), It.IsAny<object>(), It.IsAny<MemoryCacheEntryOptions>()), Times.Never);
+    }
+}
